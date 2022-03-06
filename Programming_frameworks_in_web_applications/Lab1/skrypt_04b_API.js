2c397b8552334704a939e2cb41b2c4ddd1075217
Tomasz <tomekoleszko@gmail.com>
2022-03-06 02:29:03 +0100

    lab3

--- Programming_frameworks_in_web_applications/Lab1/skrypt_04b_API.js
+++ Programming_frameworks_in_web_applications/Lab1/skrypt_04b_API.js
@@ -2,6 +2,7 @@ const express = require('express')
 const users = require('./users')
 const app = express()
 const PORT = process.env.PORT || 3000
+const uuid= require('uuid');
 
 app.get('/api/users', (req,res) => {
     res.json(users)
@@ -12,21 +13,21 @@ app.get('/api/users/:id', (req, res) => {
         res.json(users.filter(user => user.id === parseInt(req.params.id)))
     } else {
         res.status(400).json({msg: `Użytkownik o id ${req.params.id} został odnaleziony`})}
+
 })
 
 app.post('/', (req, res) => {
-
+    app.use(express.json())
     const newUser = {
-        id: req.body.id,
+        id: uuid.v4(),
         name: req.body.name,
         email: req.body.email,
         status: "aktywny"
     }
-    if (!newUser.name || !newUser.email) {
+    if(!newUser.name || !newUser.email){
         return res.status(400).json({msg: 'Wprowadź poprawne imię i nazwisko oraz email!'})
     }
     users.push(newUser)
     res.json(users)
 })
-app.use(express.json())
 app.listen(PORT, () => console.log(`Server działa na porcie: ${PORT}`))
