b1f7c887fa3f80ed4a166a1c4aa02be6c01e4e3b
Tomasz <tomekoleszko@gmail.com>
2022-04-19 22:32:16 +0200

    react auth

---
+++ Programming_frameworks_in_web_applications/Lab/Lab8/server/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(express.json())
+app.use(cors())
+const port = process.env.PORT || 8080
+const connection = require('./db')
+connection()
+const userRoutes = require("./routes/users")
+const authRoutes = require("./routes/auth")
+
+app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes)
+app.listen(port, () => console.log(`Nasłuchiwanie na porcie ${port}`))
